146272598c3039707904007e5481ec8de534383d
Weston C. Beecroft <westoncb@gmail.com>
2020-03-29 12:54:13 -0700

    Working ModelFactory

---
+++ src/ModelFactory.js
@@ -0,0 +1,87 @@
+import { MD2CharacterComplex } from 'three/examples/jsm/misc/MD2CharacterComplex.js';
+
+const MD2_SCALE = 4
+const MD2_CONTROLS = {
+    moveForward: false,
+    moveBackward: false,
+    moveLeft: false,
+    moveRight: false,
+    crouch: false,
+    jump: false,
+    attack: false
+}
+
+class ModelFactory {
+    static base
+    static skinCount
+    static instanceIndex = 1
+
+    static init() {
+        
+
+        const configOgro = {
+
+            baseUrl: "models/ogro/",
+
+            body: "ogro.md2",
+            skins: ["grok.jpg", "ogrobase.png", "arboshak.png", "ctf_r.png", "ctf_b.png", "darkam.png", "freedom.png",
+                "gib.png", "gordogh.png", "igdosh.png", "khorne.png", "nabogro.png",
+                "sharokh.png"],
+            weapons: [["weapon.md2", "weapon.jpg"]],
+            animations: {
+                move: "run",
+                idle: "stand",
+                jump: "jump",
+                attack: "attack",
+                crouchMove: "cwalk",
+                crouchIdle: "cstand",
+                crouchAttach: "crattack"
+            },
+
+            walkSpeed: 350,
+            crouchSpeed: 175
+
+        }
+
+        this.skinCount = configOgro.skins.length
+
+        this.base = new MD2CharacterComplex()
+        this.base.scale = MD2_SCALE;
+        this.basePromise = new Promise((resolve, reject) => {
+            this.base.onLoadComplete = () => {
+                this.base.enableShadows(true)
+                this.base.setWeapon(0);
+                this.base.setSkin(2);
+                resolve(this.base)
+            }
+        })
+        this.base.loadParts(configOgro);
+    }
+
+    static async getInstance(skin = this.instanceIndex+=2 % this.skinCount) {
+        const instance = new MD2CharacterComplex()
+        instance.scale = MD2_SCALE
+        instance.controls = this.getControls()
+
+        console.log("getting skin: ", skin)
+
+        return await this.basePromise.then(base => {
+            instance.shareParts(base)
+            instance.enableShadows(true);
+            instance.setWeapon(0)
+            instance.setSkin(skin)
+
+            return instance
+        })
+    }
+
+    static getControls() {
+        const controls = {}
+        Object.keys(MD2_CONTROLS).forEach(key => controls[key] = MD2_CONTROLS[key])
+        return controls
+    }
+}
+
+ModelFactory.init()
+
+export default ModelFactory
--- src/Player.js
+++ src/Player.js
@@ -2,71 +2,31 @@ import React, { useMemo, useState } from 'react'
 import { MD2CharacterComplex } from 'three/examples/jsm/misc/MD2CharacterComplex.js';
 import { useFrame } from 'react-three-fiber'
 import { Clock } from 'three'
+import ModelFactory from './ModelFactory'
 import Util from './Util'
 
 const clock = new Clock();
 
 function Player({ player}) {
-    const [md2Controls, setMd2Controls] = useState({
-        moveForward: false,
-        moveBackward: false,
-        moveLeft: false,
-        moveRight: false,
-        crouch: false,
-        jump: false,
-        attack: false
-    })
+    const [md2, setMd2] = useState(null)
 
-    const md2 = useMemo(() => {
-        const configOgro = {
-
-            baseUrl: "models/ogro/",
-
-            body: "ogro.md2",
-            skins: ["grok.jpg", "ogrobase.png", "arboshak.png", "ctf_r.png", "ctf_b.png", "darkam.png", "freedom.png",
-                "gib.png", "gordogh.png", "igdosh.png", "khorne.png", "nabogro.png",
-                "sharokh.png"],
-            weapons: [["weapon.md2", "weapon.jpg"]],
-            animations: {
-                move: "run",
-                idle: "stand",
-                jump: "jump",
-                attack: "attack",
-                crouchMove: "cwalk",
-                crouchIdle: "cstand",
-                crouchAttach: "crattack"
-            },
-
-            walkSpeed: 350,
-            crouchSpeed: 175
-
-        };
-
-        const character = new MD2CharacterComplex()
-        character.scale = 4;
-        character.onLoadComplete = () => {
-            character.root.position.set(player.position.x, 0, player.position.z)
-            character.enableShadows(true)
-            character.setWeapon(0);
-            character.setSkin(2);
-            character.controls = md2Controls
-        }
-        character.loadParts(configOgro);
-
-        return character
+    useMemo(() => {
+        ModelFactory.getInstance().then(instance => {
+            setMd2(instance)
+        })
     }, [])
 
     useFrame(() => {
         var delta = clock.getDelta();
         md2.root.position.set(player.position.x, player.position.y, player.position.z)
-
-        md2.controls = md2Controls
         md2.update(delta)
     })
 
     return (
         <mesh>
-            <primitive object={md2.root} />
+            {md2 && 
+                <primitive object={md2.root} />
+            }
         </mesh>
     )
 }
