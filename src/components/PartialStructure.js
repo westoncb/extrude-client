21681b98d1c1ce8e332f78b4a595767b013867b9
Weston C. Beecroft <westoncb@gmail.com>
2020-04-08 03:50:26 -0700

    Store 'partial points' and cellSize in global state

--- src/components/PartialStructure.js
+++ src/components/PartialStructure.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { Vector3, Vector2, Object3D } from 'three'
+import { Vector3, Vector2 } from 'three'
 import { extend, useThree, useUpdate, useFrame } from "react-three-fiber"
 import { Line2 } from 'three/examples/jsm/lines/Line2.js'
 import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial.js'
@@ -9,6 +9,7 @@ import Util from '../Util'
 import TangentGrid from './TangentGrid'
 import { mousePos} from '../global'
 import { intersections } from '../global'
+import usePlayground from '../playground'
 
 extend({ LineMaterial, LineGeometry, Line2 })
 
@@ -16,15 +17,19 @@ const MAX_POLY_POINTS = 50
 const SNAP_RADIUS = 22
 
 function PartialStructure({player, finishStructureFunc}) {
-    const [points, setPoints] = useState([])
     const [cursorPoint, setCursorPoint] = useState(null)
     const [inSnapRange, setInSnapRange] = useState(false)
     const [gridConfig, setGridConfig] = useState({position: new Vector3(), target: new Vector2(), orientation: new Vector3(), mouse: new Vector2()})
     const [parentNormal, setParentNormal] = useState(new Vector3())
     const [parentObject, setParentObject] = useState(null)
     const { size } = useThree()
+    const { state, dispatch } = usePlayground()
 
-    const visible = points.length > 0
+    const visible = state.partialPoints.length > 0
+    
+    useEffect(() => {
+        dispatch({ type: "update_cell_size", cellSize: 40})
+    }, [dispatch])
 
     const updateGridConfig = (e, points) => {
         const rotation = e.object.rotation.clone()
@@ -42,7 +47,7 @@ function PartialStructure({player, finishStructureFunc}) {
             orientation: normal.clone(),
             target: e.point.clone(),
             mouse: new Vector2(mousePos.x, mousePos.y),
-            cellSize: 40,
+            cellSize: state.cellSize,
             targetUV: e.uv.clone()
         })
     }
@@ -56,9 +61,9 @@ function PartialStructure({player, finishStructureFunc}) {
             if (inter[0] && inter[0].face) {
                 setCursorPoint(inter[0].point.clone())
 
-                if (points.length > 0) {
+                if (state.partialPoints.length > 0) {
                     if (cursorPoint) {
-                        updateGridConfig(inter[0], points)
+                        updateGridConfig(inter[0], state.partialPoints)
                     }
                 }
             }
@@ -77,34 +82,34 @@ function PartialStructure({player, finishStructureFunc}) {
             const rotation = e.object.rotation.clone()
             const normal = e.face.normal.clone().applyEuler(rotation)
 
-            if (points.length === 0) {
+            if (state.partialPoints.length === 0) {
                 console.log("setting that parent normal", normal)
                 setParentNormal(normal.clone())
                 setParentObject(e.object)
             } else {
-                console.log("points", points)
+                console.log("state.partialPoints", state.partialPoints)
             }
 
             let finish = false
-            if (points.length > 2) {
-                const snapDiffVec = vec.clone().sub(points[0])
+            if (state.partialPoints.length > 2) {
+                const snapDiffVec = vec.clone().sub(state.partialPoints[0])
                 if (snapDiffVec.length() < SNAP_RADIUS) {
                     vec.add(snapDiffVec)
                     finish = true
                 }
             }
 
-            const newPoints = [...points, vec]
-            setPoints(newPoints)
+            const newPoints = [...state.partialPoints, vec]
+            dispatch({ type: "update_partial_points", points: newPoints })
 
             updateGridConfig(e, newPoints)
 
             if (finish) {
 
-                const structure = { id: Util.generateId(), owner: player.id, points, normal, extrusionParams: { depth: 4, row: 0, theta: 0, bevelThickness: 3, bevelSize: 4, bevelSegments: 4, steps: 1} }
+                const structure = { id: Util.generateId(), owner: player.id, points: state.partialPoints, normal, extrusionParams: { depth: 4, row: 0, theta: 0, bevelThickness: 3, bevelSize: 4, bevelSegments: 4, steps: 1} }
 
                 finishStructureFunc(structure)
-                setPoints([])
+                dispatch({ type: "update_partial_points", points: [] })
                 setInSnapRange(false)
                 setCursorPoint(null)
             }
@@ -119,17 +124,17 @@ function PartialStructure({player, finishStructureFunc}) {
             MeshEvents.removeListener("partial_structure")
         }
 
-    }, [points])
+    }, [state.partialPoints])
 
     const ref = useUpdate(geom => {
-        if (points.length < 1 || cursorPoint === null) {
+        if (state.partialPoints.length < 1 || cursorPoint === null) {
             geom.setPositions(new Float32Array(MAX_POLY_POINTS*3))
             return
         }
 
         const modCursorPoint = cursorPoint.clone()
-        if (points.length > 2) {
-            const snapDiffVec = points[0].clone().sub(cursorPoint)
+        if (state.partialPoints.length > 2) {
+            const snapDiffVec = state.partialPoints[0].clone().sub(cursorPoint)
 
             // within snap radius
             if (snapDiffVec.length() < SNAP_RADIUS) {
@@ -140,15 +145,15 @@ function PartialStructure({player, finishStructureFunc}) {
             }
 
             // Check if first and last points overlap
-            if (points[0].clone().sub(points[points.length - 1]).length() < 0.1) {
-                finishStructureFunc(points)
-                setPoints([])
+            if (state.partialPoints[0].clone().sub(state.partialPoints[state.partialPoints.length - 1]).length() < 0.1) {
+                finishStructureFunc(state.partialPoints)
+                dispatch({ type: "update_partial_points", points: [] })
                 setInSnapRange(false)
                 setCursorPoint(null)
             }
         }
             
-        const finalPoints = points.reduce((acc, { x, y, z }) => [...acc, x, y, z], [], [])
+        const finalPoints = state.partialPoints.reduce((acc, { x, y, z }) => [...acc, x, y, z], [], [])
         finalPoints.push(modCursorPoint.x, modCursorPoint.y, modCursorPoint.z)
 
         // The number of points must always be exactly the same,
@@ -160,16 +165,16 @@ function PartialStructure({player, finishStructureFunc}) {
         finalPoints.length = Math.min(MAX_POLY_POINTS*3, finalPoints.length)
 
         geom.setPositions(finalPoints)
-    }, [points, cursorPoint])
+    }, [state.partialPoints, cursorPoint])
 
     return  (
         <>
-            <line2>
+            <line>
                 <lineGeometry attach="geometry" ref={ref} />
-                <lineMaterial attach="material" color={0x66ff11} linewidth={10} resolution={[size.width, size.height]} />
-            </line2>
+                <lineMaterial attach="material" color={0x66ff11} linewidth={1} resolution={[size.width, size.height]} />
+            </line>
 
-            {points.map((point, i) => (
+            {state.partialPoints.map((point, i) => (
                 <mesh key={point.x+point.y+point.z+""} castShadow position={[point.x, point.y, point.z]}>
                     <sphereGeometry attach="geometry" args={[(i === 0 && inSnapRange) ? SNAP_RADIUS : 7, 32, 32]} />
                     <meshPhysicalMaterial attach="material" color={(i === 0 && inSnapRange) ? 0x00ff00 : 0x117700} clearcoat metalness={0.25} clearcoatRoughness={0.75} roughness={0.1}/>
