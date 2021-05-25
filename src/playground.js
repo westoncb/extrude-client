96a505fb6d4b821df67ee66563fa0c519b2a9c63
Weston C. Beecroft <westoncb@gmail.com>
2021-05-24 21:15:32 -0700

    vscode formatting

--- src/playground.js
+++ src/playground.js
@@ -1,9 +1,9 @@
 import io from "socket.io-client"
-import Const from './constants'
-import {throttle} from 'lodash-es'
+import Const from "./constants"
+import { throttle } from "lodash-es"
 import { useRef, useEffect, useReducer, useMemo } from "react"
 
-const isProduction = process.env.NODE_ENV !== 'development'
+const isProduction = process.env.NODE_ENV !== "development"
 const port = 3000
 const devURL = "localhost:" + port
 const prodURL = "https://nameless-depths-23573.herokuapp.com"
@@ -11,48 +11,145 @@ const serverURL = isProduction ? prodURL : devURL
 const PERIODIC_SYNC_TIME = 2000
 const TARGET_UPDATE_THROTTLE = 1000
 
-const initialState = { players: {}, structures: {}, messages: [], partialPoints: [], cellSize: Const.BASE_CELL_SIZE}
-const RECOGNIZED_KEYS = ["a", "w", "s", "d", "f", "e", "q", " ", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
+const initialState = {
+    players: {},
+    structures: {},
+    messages: [],
+    partialPoints: [],
+    cellSize: Const.BASE_CELL_SIZE,
+}
+const RECOGNIZED_KEYS = [
+    "a",
+    "w",
+    "s",
+    "d",
+    "f",
+    "e",
+    "q",
+    " ",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+]
 
 function reducer(state, action) {
-    let playerId = action.playerId || (action.player ? action.player.id : undefined)
+    let playerId =
+        action.playerId || (action.player ? action.player.id : undefined)
     let player = playerId ? state.players[playerId] : undefined
-    
 
     switch (action.type) {
         case "update_player":
-            console.assert(playerId !== undefined, "Undefined playerId; required for:", action)
-            return {...state, players: {...state.players, [playerId]: action.player}}
+            console.assert(
+                playerId !== undefined,
+                "Undefined playerId; required for:",
+                action
+            )
+            return {
+                ...state,
+                players: { ...state.players, [playerId]: action.player },
+            }
         case "remove_player":
-            console.assert(playerId !== undefined, "Undefined playerId; required for:", action)
+            console.assert(
+                playerId !== undefined,
+                "Undefined playerId; required for:",
+                action
+            )
             const tempState = { ...state }
             delete tempState.players[playerId]
             return tempState
         case "update_local_player":
-            console.assert(playerId !== undefined, "Undefined playerId; required for:", action)
-            return { ...state, localPlayerId: playerId, players: { ...state.players, [playerId]: action.player } }
+            console.assert(
+                playerId !== undefined,
+                "Undefined playerId; required for:",
+                action
+            )
+            return {
+                ...state,
+                localPlayerId: playerId,
+                players: { ...state.players, [playerId]: action.player },
+            }
         case "update_structure":
-            return { ...state, structures: { ...state.structures, [action.structure.id]: action.structure } }
+            return {
+                ...state,
+                structures: {
+                    ...state.structures,
+                    [action.structure.id]: action.structure,
+                },
+            }
         case "replace_state":
             return action.state
         case "update_player_key_state":
-            console.assert(playerId !== undefined, "Undefined playerId; required for:", action)
-            return { ...state, players: { ...state.players, [playerId]: { ...player, keyStates: { ...player.keyStates, [action.key]: action.state}}}}
+            console.assert(
+                playerId !== undefined,
+                "Undefined playerId; required for:",
+                action
+            )
+            return {
+                ...state,
+                players: {
+                    ...state.players,
+                    [playerId]: {
+                        ...player,
+                        keyStates: {
+                            ...player.keyStates,
+                            [action.key]: action.state,
+                        },
+                    },
+                },
+            }
         case "add_player_message":
-            console.assert(playerId !== undefined, "Undefined playerId; required for:", action)
-            return { ...state, messages: [...state.messages, action.message], players: { ...state.players, [playerId]: { ...player, visibleMessages: [...player.visibleMessages, action.message] } } }
+            console.assert(
+                playerId !== undefined,
+                "Undefined playerId; required for:",
+                action
+            )
+            return {
+                ...state,
+                messages: [...state.messages, action.message],
+                players: {
+                    ...state.players,
+                    [playerId]: {
+                        ...player,
+                        visibleMessages: [
+                            ...player.visibleMessages,
+                            action.message,
+                        ],
+                    },
+                },
+            }
         case "remove_oldest_player_message":
-            console.assert(playerId !== undefined, "Undefined playerId; required for:", action)
+            console.assert(
+                playerId !== undefined,
+                "Undefined playerId; required for:",
+                action
+            )
             if (player) {
                 const visibleMessages = player.visibleMessages.slice()
                 visibleMessages.shift()
-                return { ...state, players: { ...state.players, [playerId]: { ...player, visibleMessages } } }
+                return {
+                    ...state,
+                    players: {
+                        ...state.players,
+                        [playerId]: { ...player, visibleMessages },
+                    },
+                }
             } else {
                 return state
             }
         case "update_player_target":
-            console.assert(playerId !== undefined, "Undefined playerId; required for:", action)
-            return { ...state, players: { ...state.players, [playerId]: { ...player, target: action.target} } }
+            console.assert(
+                playerId !== undefined,
+                "Undefined playerId; required for:",
+                action
+            )
+            return {
+                ...state,
+                players: {
+                    ...state.players,
+                    [playerId]: { ...player, target: action.target },
+                },
+            }
         case "update_partial_points":
             return { ...state, partialPoints: action.points.slice() }
         case "update_cell_size":
@@ -75,7 +172,10 @@ function usePlayground() {
             initSocketEventHandlers(socket, state, dispatch)
 
             const periodicSyncInterval = setInterval(() => {
-                socketRef.current.emit("event", { type: "update_player", player: localPlayer })
+                socketRef.current.emit("event", {
+                    type: "update_player",
+                    player: localPlayer,
+                })
             }, PERIODIC_SYNC_TIME)
 
             return () => {
@@ -85,19 +185,27 @@ function usePlayground() {
     }, [state])
 
     const throttledEmit = useMemo(() => {
-        return throttle(data => socketRef.current.emit("event", data, TARGET_UPDATE_THROTTLE))
+        return throttle(data =>
+            socketRef.current.emit("event", data, TARGET_UPDATE_THROTTLE)
+        )
     }, [])
 
     const execute = useMemo(() => {
         return (actionType, data) => {
-            executeAction({type: actionType, ...data}, socketRef, state, dispatch, throttledEmit)
+            executeAction(
+                { type: actionType, ...data },
+                socketRef,
+                state,
+                dispatch,
+                throttledEmit
+            )
         }
     }, [dispatch, state, throttledEmit])
 
     return {
         execute,
         state,
-        dispatch
+        dispatch,
     }
 }
 
@@ -116,11 +224,19 @@ function initSocketEventHandlers(socket, state, dispatch) {
     socket.on("disconnect", data => {
         // This is a hack: it's not a server event, but we
         // want to use the same logic for now
-        handleServerEvent({ type: "player_exit", player: localPlayer }, state, dispatch, socket)
+        handleServerEvent(
+            { type: "player_exit", player: localPlayer },
+            state,
+            dispatch,
+            socket
+        )
     })
 
     socket.on("reconnect", data => {
-        socket.emit("event", { type: "player_enter_request", player: localPlayer })
+        socket.emit("event", {
+            type: "player_enter_request",
+            player: localPlayer,
+        })
     })
 }
 
@@ -129,56 +245,88 @@ function executeAction(action, socketRef, state, dispatch, throttledEmit) {
     const localPlayerId = state.localPlayerId
     const localPlayer = localPlayerId ? state.players[localPlayerId] : undefined
 
-    console.assert(socketRef.current || action.type === "initialize", "tried executing action before initialization: ", action.type)
+    console.assert(
+        socketRef.current || action.type === "initialize",
+        "tried executing action before initialization: ",
+        action.type
+    )
 
     // console.log("execute_action: ", action)
 
     switch (action.type) {
         case "initialize":
-
             socketRef.current = io(serverURL)
-            dispatch({type: "update_local_player", player: action.player})
-            socketRef.current.emit("event", { type: "player_enter_request", player: action.player })
-            break;
+            dispatch({ type: "update_local_player", player: action.player })
+            socketRef.current.emit("event", {
+                type: "player_enter_request",
+                player: action.player,
+            })
+            break
         case "key_down":
+            if (!RECOGNIZED_KEYS.includes(action.key)) return
 
-            if (!RECOGNIZED_KEYS.includes(action.key))
-                return
-
-            dispatch({ type: "update_player_key_state", key: action.key, state: true, playerId: localPlayerId})
-            socket.emit("event", { type: "input_key_down", playerId: localPlayerId, key: action.key })
-            break;
+            dispatch({
+                type: "update_player_key_state",
+                key: action.key,
+                state: true,
+                playerId: localPlayerId,
+            })
+            socket.emit("event", {
+                type: "input_key_down",
+                playerId: localPlayerId,
+                key: action.key,
+            })
+            break
         case "key_up":
+            if (!RECOGNIZED_KEYS.includes(action.key)) return
 
-            if (!RECOGNIZED_KEYS.includes(action.key))
-                return
-
-            dispatch({ type: "update_player_key_state", key: action.key, state: false, playerId: localPlayerId})
-            socket.emit("event", { type: "input_key_up", playerId: localPlayerId, key: action.key })
-            break;
+            dispatch({
+                type: "update_player_key_state",
+                key: action.key,
+                state: false,
+                playerId: localPlayerId,
+            })
+            socket.emit("event", {
+                type: "input_key_up",
+                playerId: localPlayerId,
+                key: action.key,
+            })
+            break
         case "update_player_target":
-            dispatch({ type: "update_player_target", playerId: localPlayerId, target: action.target })
-            throttledEmit({ type: "player_target_change", playerId: localPlayerId, target: action.target })
-            break;
+            dispatch({
+                type: "update_player_target",
+                playerId: localPlayerId,
+                target: action.target,
+            })
+            throttledEmit({
+                type: "player_target_change",
+                playerId: localPlayerId,
+                target: action.target,
+            })
+            break
         case "send_chat_message":
-            socket.emit("event", { type: "chat_message", message: action.message, player: localPlayer, time: new Date() })
-            break;
+            socket.emit("event", {
+                type: "chat_message",
+                message: action.message,
+                player: localPlayer,
+                time: new Date(),
+            })
+            break
         case "update_structure":
             dispatch(action)
-            socket.emit("event", {...action, playerId: localPlayerId})
-            break;
+            socket.emit("event", { ...action, playerId: localPlayerId })
+            break
         case "update_player":
             dispatch(action)
             socket.emit("event", action)
-            break;
+            break
         default:
             new Error("unrecognized action:", action)
-            break;
+            break
     }
 }
 
 function handleServerEvent(event, state, dispatch, socket) {
-
     const localPlayerId = state.localPlayerId
     const localPlayer = localPlayerId ? state.players[localPlayerId] : undefined
 
@@ -187,69 +335,93 @@ function handleServerEvent(event, state, dispatch, socket) {
     switch (event.type) {
         case "player_enter":
             console.log("player_enter: ", event.player)
-            dispatch({type: "update_player", player: event.player})
-            break;
+            dispatch({ type: "update_player", player: event.player })
+            break
         case "player_exit":
             console.log("player_exit: ", event.player)
-            if (state.players[event.player.id] && event.player.id !== localPlayerId) {
-                dispatch({ type: "remove_player", playerId: event.player.id})
+            if (
+                state.players[event.player.id] &&
+                event.player.id !== localPlayerId
+            ) {
+                dispatch({ type: "remove_player", playerId: event.player.id })
             }
-            break;
+            break
         case "full_state_request":
             socket.emit("event", { type: "full_state_response", state })
-            break;
+            break
         case "full_state_update":
-            dispatch({ type: "replace_state", state: { ...event.state, localPlayerId}})
-            break;
+            dispatch({
+                type: "replace_state",
+                state: { ...event.state, localPlayerId },
+            })
+            break
         case "input_key_down":
-            if (event.playerId === localPlayerId)
-                return
+            if (event.playerId === localPlayerId) return
 
-            dispatch({ type: "update_player_key_state", key: event.key, state: true, playerId: event.playerId})
-            break;
+            dispatch({
+                type: "update_player_key_state",
+                key: event.key,
+                state: true,
+                playerId: event.playerId,
+            })
+            break
         case "input_key_up":
-            if (event.playerId === localPlayerId)
-                return
+            if (event.playerId === localPlayerId) return
 
-            dispatch({ type: "update_player_key_state", key: event.key, state: false, playerId: event.playerId })
-            break;
+            dispatch({
+                type: "update_player_key_state",
+                key: event.key,
+                state: false,
+                playerId: event.playerId,
+            })
+            break
         case "chat_message":
-
-            if ((event.playerId && !state.players[event.playerId])) {
+            if (event.playerId && !state.players[event.playerId]) {
                 break
             }
 
-
-            const messageDisplayTime = 5000 + (1000 * Math.floor(event.message.length / 100))
-            dispatch({ type: "add_player_message", playerId: event.player.id, message: {...event}})
+            const messageDisplayTime =
+                5000 + 1000 * Math.floor(event.message.length / 100)
+            dispatch({
+                type: "add_player_message",
+                playerId: event.player.id,
+                message: { ...event },
+            })
 
             setTimeout(() => {
-                dispatch({ type: "remove_oldest_player_message", playerId: event.player.id})
+                dispatch({
+                    type: "remove_oldest_player_message",
+                    playerId: event.player.id,
+                })
             }, messageDisplayTime)
-            
-            break;
+
+            break
         case "player_target_change":
-            if ((event.playerId && !state.players[event.playerId]) || event.playerId === state.localPlayerId) {
+            if (
+                (event.playerId && !state.players[event.playerId]) ||
+                event.playerId === state.localPlayerId
+            ) {
                 break
             }
 
-            dispatch({type: "update_player_target", playerId: event.playerId, target: event.target})
-            break;
+            dispatch({
+                type: "update_player_target",
+                playerId: event.playerId,
+                target: event.target,
+            })
+            break
         case "update_player":
-
-            if (event.player.id === localPlayerId)
-                break
-            dispatch({type: "update_player", player: event.player})
-            break;
+            if (event.player.id === localPlayerId) break
+            dispatch({ type: "update_player", player: event.player })
+            break
         case "update_structure":
-            if (event.playerId === localPlayerId)
-                break
-            dispatch({ type: "update_structure", structure: event.structure})
-            break;
+            if (event.playerId === localPlayerId) break
+            dispatch({ type: "update_structure", structure: event.structure })
+            break
         default:
             console.log("unrecognized server event: ", event)
-            break;
-        }
+            break
+    }
 }
 
 export default usePlayground
